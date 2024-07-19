e3f1d9e9c95cf6288becd57050bb10d6ea938023
Cristobal <quilimacox1@gmail.com>
2024-07-18 20:26:56 -0500

    Inicio de ejercicio

---
+++ ejerciciosconclass.py
@@ -0,0 +1,6 @@
+class Libro:
+    def __init__(self, titulo, autor, anio_publicacion, isbn):
+        self.titulo = titulo
+        self.autor = autor
+        self.anio_publicaion = anio_publicacion
+        
