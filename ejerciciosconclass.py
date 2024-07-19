9194f61c94c8844c8ae8623cf79cf5d837b80472
Cristobal <quilimacox1@gmail.com>
2024-07-18 20:33:24 -0500

    Se agrega termina clase miembro

--- ejerciciosconclass.py
+++ ejerciciosconclass.py
@@ -3,4 +3,17 @@ class Libro:
         self.titulo = titulo
         self.autor = autor
         self.anio_publicaion = anio_publicacion
-        
+        self.isbn = isbn
+        self.prestado = False
+
+    def prestamo(self):
+        self.prestamo = True
+
+    def devolucion(self):
+        self.prestamo = False
+
+class Miembro:
+    def __init__(self, nombre, numero_miembro):
+        self.nombre = nombre
+        self.numero_miembre = numero_miembro
+        self.libros_prestados = []
