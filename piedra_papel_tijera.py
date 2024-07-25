80bc34fbe6e81ea2cc9546efcc368b92e1dd0408
Cristobal <quilimacox1@gmail.com>
2024-07-24 20:41:44 -0500

    Se inicia juego de piedra papel o tijera

---
+++ piedra_papel_tijera.py
@@ -0,0 +1,13 @@
+#Creea un juego de piedra papel o tijera
+
+"""Definir las opciones y reglas del juego:
+
+Piedra vence a tijera
+Tijera vence a papel
+Papel vence a piedra
+Implementar la lógica del juego:
+
+Pedir al jugador que elija entre piedra, papel o tijera.
+Generar una elección aleatoria para la computadora.
+Comparar las elecciones para determinar el ganador.
+Mostrar el resultado y preguntar al jugador si desea jugar de nuevo."""
