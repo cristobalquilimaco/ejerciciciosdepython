bb6ef0db205df642f988b5d617daaf0380d80227
Cristobal <quilimacox1@gmail.com>
2024-11-08 22:51:27 -0500

    Se inicia case de match

--- ejercicio.py
+++ ejercicio.py
@@ -5,8 +5,37 @@ print("3. Crear una nueva rama")
 print("4. Cambiar de rama")
 print("5. Mostrar ficheros pendientes de hacer commit")
 print("6. Hacer commit (+add)")
-print("\nGit y gitHub CLI - Opciones:")
-print("\nGit y gitHub CLI - Opciones:")
-print("\nGit y gitHub CLI - Opciones:")
-print("\nGit y gitHub CLI - Opciones:")
-print("\nGit y gitHub CLI - Opciones:")
+print("7. Mostrar el historial de commits")
+print("8. Eliminar rama")
+print("9. Establecer repositorio remoto")
+print("10. Hacer pull")
+print("11. Hacer push")
+print("12. Salir")
+
+choice = input("selecciona una opcion (1 al 12): ")\
+
+match choice:
+    case "1":
+        pass
+    case "2":
+        pass
+    case "3":
+        pass
+    case "4":
+        pass
+    case "5":
+        pass
+    case "6":
+        pass
+    case "7":
+        pass
+    case "8":
+        pass
+    case "9":
+        pass
+    case "10":
+        pass
+    case "11":
+        pass
+    case "12":
+        pass
