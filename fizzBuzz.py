6278b24f73fdf054e14cd8b7fc80066410c5a8a1
Cristobal <quilimacox1@gmail.com>
2024-12-21 20:01:41 -0500

    Se actualiza funcion y se colocan condicionales

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -9,6 +9,12 @@
 # Imprimir el número si no es divisible por 3 ni por 5.
 
 def FizzBuzz():
-    for num in range(1, 100):
-        if num % 3 == 0:
-            print("Fizz") 
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
