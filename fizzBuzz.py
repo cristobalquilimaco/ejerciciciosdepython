225ea58c11382c341e6fc680cc8473604e1598d8
Cristobal <quilimacox1@gmail.com>
2024-12-21 19:56:51 -0500

    Se actualiza inicia nuevo ejercicio de logica fizzBuzz

---
+++ fizzBuzz.py
@@ -0,0 +1,9 @@
+# Ejercicio: Fizz Buzz
+# Escribe un programa en Python que haga lo siguiente:
+
+# Iterar sobre los números del 1 al 100 (ambos inclusive).
+# Para cada número:
+# Imprimir "Fizz" si el número es divisible por 3.
+# Imprimir "Buzz" si el número es divisible por 5.
+# Imprimir "FizzBuzz" si el número es divisible por ambos (3 y 5).
+# Imprimir el número si no es divisible por 3 ni por 5.
