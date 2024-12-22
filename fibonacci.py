88c1ad617fb1cf5b84a41e5bbc41aac9121dee80
Cristobal <quilimacox1@gmail.com>
2024-12-22 14:41:16 -0500

    Se crea enunciado para el ejercicio

---
+++ fibonacci.py
@@ -0,0 +1,15 @@
+# Enunciado del ejercicio:
+
+# La sucesión de Fibonacci es una serie de números donde cada número es la suma de los dos anteriores. 
+# La secuencia comienza con 0 y 1,
+
+# Tu tarea es escribir un programa en Python que:
+
+# Solicite al usuario un número entero 
+# 𝑛
+# n.
+# Calcule los primeros 
+# 𝑛
+# n números de la sucesión de Fibonacci.
+# Imprima la secuencia resultante.
+
