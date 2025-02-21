af269f7837da8b50db3b420aefa0be2aa90af58a
Cristobal <quilimacox1@gmail.com>
2025-02-20 20:29:52 -0500

    Se agrega funcion para validar si el libro esta disponible

--- biblioteca.py
+++ biblioteca.py
@@ -2,5 +2,10 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
+        self.available = True
 
+    def borrow(self):
+        if self.available:
+            self.available = False
+            print(f"El libro {self.title} ha sido prestado")
     
