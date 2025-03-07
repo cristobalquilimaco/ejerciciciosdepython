1b6a5013c489a020e90eb3c442164cf4aa0efa4b
Cristobal <quilimacox1@gmail.com>
2025-03-07 18:52:12 -0500

    Se crea ejercicio contar palabras

---
+++ contarPalabras.py
@@ -0,0 +1,7 @@
+# Escribe una función llamada contar_palabras que reciba una cadena de texto como argumento y devuelva un diccionario donde las claves sean las palabras únicas y los valores representen la cantidad de veces que aparece cada palabra en el texto.
+
+# Requisitos:
+
+# Ignorar mayúsculas y minúsculas (es decir, "Hola" y "hola" cuentan como la misma palabra).
+# Ignorar signos de puntuación como comas, puntos, signos de exclamación o interrogación.
+# Devolver el diccionario ordenado alfabéticamente por las palabras.
