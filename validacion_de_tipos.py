e20ae5bddabde3df5c0c4c16017719b38e09629c
Cristobal <quilimacox1@gmail.com>
2025-04-14 23:06:50 -0500

    Se agrega validacion de datos para cada instancia

--- validacion_de_tipos.py
+++ validacion_de_tipos.py
@@ -1,4 +1,10 @@
-def divide(a: int, b: int) -> float:
-    return a/b
+# def divide(a: int, b: int) -> float:
+#     return a/b
 
-print(divide(10, "2"))
+# print(divide(10, "2"))
+
+def divide(a: int, b: int) -> float:
+
+    if isinstance(a, int) or not isinstance(b, int):
+        print('Error: ambos parametros deben ser enteros o flotantes')
+        return None
