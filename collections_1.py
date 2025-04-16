81ec0326a109db6392a9610a893e801c847f3301
Cristobal <quilimacox1@gmail.com>
2025-04-15 23:05:38 -0500

    Se retorna vaor de product_count

--- collections_1.py
+++ collections_1.py
@@ -5,4 +5,4 @@ def count_products(orders: list[str]) -> defaultdict:
     product_count = defaultdict(int)
     for product in orders:
         product_count[product] +=1
-        
+        return product_count
