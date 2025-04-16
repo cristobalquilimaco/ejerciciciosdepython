a6c39b7c0393e4fd34c01f2582fc08f99e908828
Cristobal <quilimacox1@gmail.com>
2025-04-15 23:05:04 -0500

    Se agrega loop para recorrer los productos

--- collections_1.py
+++ collections_1.py
@@ -3,4 +3,6 @@ from collections import defaultdict
 def count_products(orders: list[str]) -> defaultdict:
     #crea un idccionario con valor por defecto 0
     product_count = defaultdict(int)
-    
+    for product in orders:
+        product_count[product] +=1
+        
