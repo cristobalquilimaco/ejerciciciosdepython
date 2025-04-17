3a81fb7065ac39ac517473e22248cb9e0db364c9
Cristobal <quilimacox1@gmail.com>
2025-04-17 11:21:44 -0500

    Se retorna valor de la cola

--- manage_delivery_queue.py
+++ manage_delivery_queue.py
@@ -4,3 +4,6 @@ def manage_delivery_queue() -> deque:
     # Crea una cola para gestionar entregas de productos
     delivery_queue = deque(["order_1", "order_2", "order_3"])
     delivery_queue.append("order_4") #Agrega valor al final de la cola
+    delivery_queue.appendleft("order_0") #Grega valor al principio de la cola
+    return delivery_queue
+
