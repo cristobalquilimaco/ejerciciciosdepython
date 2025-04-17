cde88c7b93d8e3287eb21491bbf9d44ed831da34
Cristobal <quilimacox1@gmail.com>
2025-04-17 11:19:03 -0500

    Se agrega funcion manage_delivery_queue

--- manage_delivery_queue.py
+++ manage_delivery_queue.py
@@ -1,2 +1,5 @@
 from collections import deque
 
+def manage_delivery_queue() -> deque:
+    # Crea una cola para gestionar entregas de productos
+    delivery_queue = deque(["order_1", "order_2", "order_3"])
