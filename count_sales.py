8f8a442a8701c83abb0ba58e32ccf39392cbc379
Cristobal <quilimacox1@gmail.com>
2025-04-17 11:09:16 -0500

    Se agrega variable con los productos vendidos

--- count_sales.py
+++ count_sales.py
@@ -3,3 +3,6 @@ from collections import Counter
 def counter_sales(products: list[str]) ->Counter:
     #Usa Counter para contar cuantos productos de cada tipo se han vendido
     return Counter(products)
+
+sales = ["laptop", "smartphone", "laptop", "tablet"]
+
