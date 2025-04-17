264265193e307fd762f9c08f28209f3c20817a44
Cristobal <quilimacox1@gmail.com>
2025-04-17 11:44:28 -0500

    Se agrega condiciones del retorno del producto

--- order_status.py
+++ order_status.py
@@ -4,3 +4,14 @@ class OrderStatus(Enum):
     PEDING = 1 #Pendiente
     SHIPPED = 2 #Enviado
     DELIVERED = 3 #Entregado
+
+def check_order_status(status: OrderStatus):
+    #Comprueba el estado del pedido y develve un mensaje
+    if status == OrderStatus.PEDING:
+        return "Order is still pending."
+    elif status == OrderStatus.SHIPPED:
+        return "Order has benn shipped."
+    elif status == OrderStatus.DELIVERED:
+        return "Order has been Delivered"
+    
+    
