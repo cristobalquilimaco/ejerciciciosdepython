96c945c8afdda504680592530e5c9540d2fb35c1
Cristobal <quilimacox1@gmail.com>
2025-04-17 11:08:07 -0500

    Se corrigen return de productos

--- count_sales.py
+++ count_sales.py
@@ -2,4 +2,4 @@ from collections import Counter
 
 def counter_sales(products: list[str]) ->Counter:
     #Usa Counter para contar cuantos productos de cada tipo se han vendido
-    return Counter[products]
+    return Counter(products)
