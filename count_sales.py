082db138822f3ef8f84485dd7c44ca38b8ec18f5
Cristobal <quilimacox1@gmail.com>
2025-04-17 11:07:28 -0500

    Se funcion para validar cuantos productos se han vendido

--- count_sales.py
+++ count_sales.py
@@ -1,1 +1,5 @@
 from collections import Counter
+
+def counter_sales(products: list[str]) ->Counter:
+    #Usa Counter para contar cuantos productos de cada tipo se han vendido
+    return Counter[products]
