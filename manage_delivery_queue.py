57eeee0ae7aa0e1ca6f749d5d6bda50c6a0fa93e
Cristobal <quilimacox1@gmail.com>
2025-04-17 11:20:23 -0500

    Se agrega valor al final de la cola con el metodo append

--- manage_delivery_queue.py
+++ manage_delivery_queue.py
@@ -3,3 +3,4 @@ from collections import deque
 def manage_delivery_queue() -> deque:
     # Crea una cola para gestionar entregas de productos
     delivery_queue = deque(["order_1", "order_2", "order_3"])
+    delivery_queue.append("order_4") #Agrega valor al final de la cola
