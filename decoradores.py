3dc91ca6e555d2fcc3e5d107a9577a8b64a7986e
Cristobal <quilimacox1@gmail.com>
2025-04-18 18:47:53 -0500

    Se documenta ejercicio

--- decoradores.py
+++ decoradores.py
@@ -1,6 +1,6 @@
 def log_transaction(func):
     def wrapper():
-        print('Log de transacción...')
+        print('1 Log de transacción...') 
         func()
         print('log terminado...')
     return wrapper
@@ -13,3 +13,9 @@ def process_payment():
     print('Procesando el pago...')
 
 process_payment()
+
+
+#Pasos:
+# Primero se Ejecuta la transacción en este caso 1 log de la transaccion
+# Segundo se Ejecuta la funcion que esta dentro del decorador
+# Y por ultimo se ejecuta el valor de la funcion iniciaal que es log de la transaccion
