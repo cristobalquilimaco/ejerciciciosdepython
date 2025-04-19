296fd81e00a75ac492a67e84e47658063b6cda53
Cristobal <quilimacox1@gmail.com>
2025-04-19 06:58:43 -0500

    feat: se agrega funcion decorador para validar si el usuario tiene el admin para eliminar a otro empleado

--- decoradoresconparametros.py
+++ decoradoresconparametros.py
@@ -1,8 +1,23 @@
 #Decorador que comprueba si un empeado tiene un rol en especifico
 
-def check_access(reuired):
+def check_access(required_role):
+    def decorator(func):
+        def wrapper(employe):
+            #Si el rol del empleado coincide con el rol requerido
+            if employe.get('role') == required_role:
+                return func(employe)
+            else:
+                print(f'ACCESO DENEGADO. Solo {required_role} pueden realizar esta accion')
 
+        return wrapper
+    return decorator
 
+def log_action(func):
+    def wrapper(employee):
+        print(f'Registrando accion para el empleado{employee['name']}')
+        return func(employee)
+    return wrapper
+    
 
 @check_access('admin')
 @log_action
