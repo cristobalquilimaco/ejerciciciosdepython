18214fd23034887b2d585e1b76c4c485a20a8bd8
Cristobal <quilimacox1@gmail.com>
2025-04-19 06:52:45 -0500

    feat: se agrega funcion para validar el rol del empleado

--- decoradoresconparametros.py
+++ decoradoresconparametros.py
@@ -1,4 +1,11 @@
-@check_acces('admin')
+#Decorador que comprueba si un empeado tiene un rol en especifico
+
+def check_access(reuired):
+
+
+
+@check_access('admin')
 @log_action
 def delete_employee(employee):
     print(f'El empleado {employee['name']}, ha sido eliminado')
+
