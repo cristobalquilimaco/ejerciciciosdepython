6fc35ac04e86d52264c3432b580c7bdf6f8d8a0f
Cristobal <quilimacox1@gmail.com>
2025-04-22 19:15:51 -0500

    Se agrega funcion para definir el metodo persona

--- person.py
+++ person.py
@@ -2,4 +2,9 @@ class Person():
     def __init__(self, nombre: str, edad:int):
         self.nombre = nombre
         self.edad = edad
-        
+
+    def __str__(self) -> str:
+        #devuelve una representacion amigable del producto
+        return f"Persona: {self.nombre}, {self.edad} años"
+    
+    
